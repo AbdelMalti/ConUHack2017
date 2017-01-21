d5cca051c02a7553a71a4293b23d9396370d6243
Abdeljelil Malti <abdeljelil.malti@outlook.com>
2017-01-21 17:55:45 -0500

    Modification de la vue

--- CardioTime2/app/src/main/java/com/malti/abdeljelil/cardiotime/MainActivity.java
+++ CardioTime2/app/src/main/java/com/malti/abdeljelil/cardiotime/MainActivity.java
@@ -2,12 +2,19 @@ package com.malti.abdeljelil.cardiotime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button_start;
+    private TextView 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        button_start= (Button) findViewById(R.id.playButton);
+
         setContentView(R.layout.activity_main);
     }
 }
