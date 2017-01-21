4f93f8b2e31833649d10e9e8337d17b3a1210454
Anisbel <anis.belkeir@gmail.com>
2017-01-21 17:19:22 -0500

    add testToSpeech function

--- CardioTime2/app/src/main/java/com/malti/abdeljelil/cardiotime/MainActivity.java
+++ CardioTime2/app/src/main/java/com/malti/abdeljelil/cardiotime/MainActivity.java
@@ -1,13 +1,67 @@
-package com.malti.abdeljelil.cardiotime;
+package com.example.admin.texttospeach;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public abstract class MainActivity extends Activity implements TextToSpeech.OnInitListener{
+
+
+
+    Button button_start,button_stop;
+    TextToSpeech tts;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test_text_to_speach);
+        //button start pour le timer
+        button_start= (Button) findViewById(R.id.button_start);
+        button_start.setOnClickListener(this);
+
+        tts= new TextToSpeech(this,this);
+
+
     }
+
+
+}
+
+public void onClick(View v){
+
+
+    //TODO generer timer
+
+    //if(variable==timer){
+
+        //tts.speak(timer)
+
+
+   // }
+
+
+
+
+
+
+
+
+
+
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            mTts.speak("Ceci est un test grandeur nature du tutoriel sur l'énonciation de texte.", TextToSpeech.QUEUE_FLUSH, null);
+            mTts.speak("Ceci est un deuxième test !", TextToSpeech.QUEUE_ADD, null);
+        }
+    }
+
+
 }
